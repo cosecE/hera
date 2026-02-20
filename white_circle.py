2dbed55e8e9f5556dd6208642f0bf7217043dd14
kaushiki singh <97705220+cosecE@users.noreply.github.com>
2026-02-20 16:52:35 -0500

    Update white_circle.py

--- white_circle.py
+++ white_circle.py
@@ -1,6 +1,7 @@
 import requests
+import os
 
-WHITE_CIRCLE_API_KEY = os.getenv("WHITE_CIRCLE_API_KEY") 
+api_key = os.getenv("WHITE_CIRCLE_API_KEY") 
 WHITE_CIRCLE_URL = "https://us.whitecircle.ai/api/session/check"
 WHITE_CIRCLE_DEPLOYMENT_ID = "455439b6-aec6-4198-a639-23441f32e8f8"
 
